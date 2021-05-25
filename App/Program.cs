45ed4f43cd3642b507a96091ca34b68f7bda0faa
LWooler <lwooler@att.net>
2021-05-25 12:19:07 -0500

    added queries to find gpa averages and highest gpa student

--- App/Program.cs
+++ App/Program.cs
@@ -20,6 +20,14 @@ namespace EFCoreGalvanizeTeam
       var student1 = db.Students.Include(student => student.Grades).Where(student => student.FirstName == "Don").FirstOrDefault();
       student1.Grades.ToList().ForEach(grade => Console.WriteLine($"{grade.Value}") );
 
+      Console.WriteLine("\nGiven a student's name find their average grade among all their courses");
+      var student2 = db.Students.Include(student => student.Grades).Where(student => student.FirstName == "Jack").FirstOrDefault();
+      var gpa = student2.Grades.Average(grade => grade.Value);
+      Console.WriteLine($"Jack Daniel's GPA = {gpa}");
+
+      Console.WriteLine("\n Find the student with the highest average grade");
+      var student3 = db.Students.Include(student => student.Grades).OrderByDescending(student => student.Grades.Average(grade => (float)grade.Value)).FirstOrDefault();
+      Console.WriteLine($"{student3.FirstName} {student3.LastName} has the highest GPA");
     }
   }
 }
