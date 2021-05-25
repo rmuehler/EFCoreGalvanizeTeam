6b88918d8a829354d2b4568de2147fe5c0bde2f9
psalaza <43583947+psalaza@users.noreply.github.com>
2021-05-25 14:05:07 -0400

    refactor to include anonymous object

--- App/Program.cs
+++ App/Program.cs
@@ -10,20 +10,20 @@ namespace EFCoreGalvanizeTeam
     static void Main(string[] args)
     {
       var db = new DatabaseContext();
-
+      //1
       Console.WriteLine("Show List of all students!");
       foreach (var student in db.Students.Select(student => new { fName = student.FirstName, lName = student.LastName }))
       {
         Console.WriteLine($"{student.fName} {student.lName}");
       }
-
+      //2
       Console.WriteLine("\nShow Don Julio's grades!");
       var student1 = db.Students.Include(student => student.Grades)
           .Where(student => student.FirstName == "Don")
           .Select(student => new { grades = student.Grades })
           .FirstOrDefault();
       student1.grades.ToList().ForEach(grade => Console.WriteLine($"{grade.Value}"));
-
+      //3
       Console.WriteLine("\nGiven a student's name find their average grade among all their courses");
       var student2 = db.Students.Include(student => student.Grades)
           .Where(student => student.FirstName == "Jack")
@@ -35,26 +35,27 @@ namespace EFCoreGalvanizeTeam
           })
           .FirstOrDefault();
       Console.WriteLine($"{student2.fName} {student2.lName}'s GPA = {student2.average}");
-
+      //4
       Console.WriteLine("\nFind the student with the highest average grade");
       var student3 = db.Students.Include(student => student.Grades)
-          .OrderByDescending(student => student.Grades.Average(grade => (float)grade.Value)).FirstOrDefault();
-      Console.WriteLine($"{student3.FirstName} {student3.LastName} has the highest GPA");
-
+          .OrderByDescending(student => student.Grades.Average(grade => (float)grade.Value)).Select(student => new {fName = student.FirstName, lName = student.LastName}).FirstOrDefault();
+      Console.WriteLine($"{student3.fName} {student3.lName} has the highest GPA");
+      //5
       Console.WriteLine("\nFind the student student with the highest number of courses");
       var student4 = db.Students.Include(student => student.Grades)
-          .OrderByDescending(student => student.Grades.Count).FirstOrDefault();
-      Console.WriteLine($"{student4.FirstName} {student4.LastName} the most number of courses");
-
-      Console.Out.WriteLine("\nFind the student that is taking not courses");
+          .OrderByDescending(student => student.Grades.Count).Select(student => new {fName = student.FirstName, lName = student.LastName}).FirstOrDefault();
+      Console.WriteLine($"{student4.fName} {student4.lName} the most number of courses");
+      //6
+      Console.Out.WriteLine("\nFind the student that is not taking any courses");
       var student5 = db.Students.Include(student => student.Grades)
-          .FirstOrDefault(student => student.Grades.Count == 0);
-      Console.Out.WriteLine($"{student5.FirstName} {student5.LastName} is taking no courses");
-
+          .Where(student => student.Grades.Count == 0)
+          .Select(student => new {fName = student.FirstName, lName = student.LastName}).FirstOrDefault();
+      Console.Out.WriteLine($"{student5.fName} {student5.lName} is taking no courses");
+      //7
       Console.Out.WriteLine("\nFind the number of Freshmen");
       var freshmenCount = db.Students.Count(student => student.Classification == Student.Class.Freshman);
       Console.Out.WriteLine($"The number of Freshmen is: {freshmenCount}");
-
+      //8
       Console.Out.WriteLine("\nFind the number of Sophomores");
       var sophomoreAverages = db.Students.Include(student => student.Grades)
           .Where(student => student.Classification == Student.Class.Sophomore).Select(student =>
