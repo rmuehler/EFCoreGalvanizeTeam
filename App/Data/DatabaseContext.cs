840e7ef62c3ef347a24afc9169e432535f74f7e5
Okinz <steven14812@gmail.com>
2021-05-25 11:46:43 -0400

    Added db data

--- App/Data/DatabaseContext.cs
+++ App/Data/DatabaseContext.cs
@@ -1,21 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using static EFCoreGalvanizeTeam.Student;
 
 namespace EFCoreGalvanizeTeam.Data
 {
-    public class DatabaseContext : DbContext
+  public class DatabaseContext : DbContext
+  {
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Grade> Grades { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=app.db");
-            base.OnConfiguring(optionsBuilder);
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+      optionsBuilder.UseSqlite("Data Source=app.db");
+      base.OnConfiguring(optionsBuilder);
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      var grades = new Grade[] {
+        new Grade { Id = 1, StudentId = 1, CourseName = "English I", Value = 90 },
+        new Grade { Id = 2, StudentId = 1, CourseName = "Calculus II", Value = 70 },
+        new Grade { Id = 3, StudentId = 2, CourseName = "Theater Appreciation", Value = 50 },
+        new Grade { Id = 4, StudentId = 2, CourseName = "Dynamic Programming", Value = 85 },
+        new Grade { Id = 5, StudentId = 2, CourseName = "Data Structures", Value = 15 },
+        new Grade { Id = 6, StudentId = 3, CourseName = "Senior Design I", Value = 97 },
+        new Grade { Id = 7, StudentId = 3, CourseName = "Cloud Computing", Value = 75 },
+        new Grade { Id = 8, StudentId = 5, CourseName = "Mixology", Value = 20 },
+        new Grade { Id = 9, StudentId = 5, CourseName = "Mixology II", Value = 95 },
+        new Grade { Id = 10, StudentId = 6, CourseName = "Mixology", Value = 16 },
+      };
+      var students = new Student[] {
+        new Student {Id = 1, FirstName = "Samuel", LastName = "Adams", Age = 14, Classification = Class.Freshman},
+        new Student {Id = 2, FirstName = "Jack", LastName = "Daniels", Age = 16, Classification = Class.Sophomore},
+        new Student {Id = 3, FirstName = "Jim", LastName = "Bean", Age = 17, Classification = Class.Senior},
+        new Student {Id = 4, FirstName = "Johnnie", LastName = "Walker", Age = 15, Classification = Class.Junior},
+        new Student {Id = 5, FirstName = "Don", LastName = "Julio", Age = 20, Classification = Class.Sophomore},
+        new Student {Id = 6, FirstName = "Jose", LastName = "Cuervo", Age = 18, Classification = Class.Senior},
+      };
+      modelBuilder.Entity<Student>().HasData(students);
+      modelBuilder.Entity<Grade>().HasData(grades);
+    }
+  }
 }
---
+++ App/Migrations/20210525154529_initial migration.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreGalvanizeTeam.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Classification = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StudentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CourseName = table.Column<string>(type: "TEXT", nullable: true),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Grades_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 1, 14, 0, "Samuel", "Adams" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 2, 16, 1, "Jack", "Daniels" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 3, 17, 3, "Jim", "Bean" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 4, 15, 2, "Johnnie", "Walker" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 5, 20, 1, "Don", "Julio" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Classification", "FirstName", "LastName" },
+                values: new object[] { 6, 18, 3, "Jose", "Cuervo" });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 1, "English I", 1, 90m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 2, "Calculus II", 1, 70m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 3, "Theater Appreciation", 2, 50m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 4, "Dynamic Programming", 2, 85m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 5, "Data Structures", 2, 15m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 6, "Senior Design I", 3, 97m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 7, "Cloud Computing", 3, 75m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 8, "Mixology", 5, 20m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 9, "Mixology II", 5, 95m });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "StudentId", "Value" },
+                values: new object[] { 10, "Mixology", 6, 16m });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grades_StudentId",
+                table: "Grades",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
